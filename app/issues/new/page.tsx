7965fe7989270a470523e18f247bc6996d8de5f2
Rika Miyata <rika13tay@gmail.com>
2023-10-11 12:42:00 +0900

    Customize Radix UI theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, TextArea, TextField } from '@radix-ui/themes';
+import React from 'react';
 
 const NewIssuePage = () => {
   return (
@@ -12,7 +12,7 @@ const NewIssuePage = () => {
       <TextArea placeholder='Description' />
       <Button>Submit New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
